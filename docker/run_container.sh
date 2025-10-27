ac6c997feba5518fb36daa6feaa3d1a1a5ba00b4
mendelas <kota4610@gmail.com>
2025-10-27 17:35:39 +0900

    change

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
+set -e
+
 xhost +local:root
+
 IMAGE="omnilrs-navigation:v1.0"
 NAME="omnilrs-navigation-container"
-DOCKER_RUN_CMD="docker run -it --rm --privileged \
-                --gpus all \
-                --runtime=nvidia \
-                -e NVIDIA_VISIBLE_DEVICES=all \
-                -e NVIDIA_DRIVER_CAPABILITIES=all \
-                -e XDG_RUNTIME_DIR=/tmp/runtime-docker \
-                -e "ACCEPT_EULA=Y" \
-                -e "PRIVACY_CONSENT=Y" \
-                -e "DISPLAY=$DISPLAY" \
-                -v $HOME/.Xauthority:/root/.Xauthority \
-                -v /dev/:/dev/ \
-                -v $PWD/docker:/docker \
-                -v $PWD/humble_ws:/ros2_ws \
-                -v omnilrs_ros2_demo_cmd:/commandhistory \
-                --network=host \
-                --ipc=host \
-                --name $NAME \
-                $IMAGE"
+RUNTIME_DIR="/tmp/runtime-docker"
 
-# Create runtime directory
-mkdir -p /tmp/runtime-docker
+mkdir -p "$RUNTIME_DIR"
 
-exec ${DOCKER_RUN_CMD[*]}
+docker run -it --rm --privileged \
+    --gpus=all \
+    -e NVIDIA_VISIBLE_DEVICES=all \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e XDG_RUNTIME_DIR="$RUNTIME_DIR" \
+    -e ACCEPT_EULA=Y \
+    -e PRIVACY_CONSENT=Y \
+    -e DISPLAY="$DISPLAY" \
+    -v "$HOME/.Xauthority:/root/.Xauthority" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /dev/:/dev/ \
+    -v "$PWD/docker:/docker" \
+    -v "$PWD/humble_ws:/ros2_ws" \
+    -v omnilrs_ros2_demo_cmd:/commandhistory \
+    --network=host \
+    --ipc=host \
+    --name "$NAME" \
+    "$IMAGE"
